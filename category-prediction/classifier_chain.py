fa2770205428910b4d081f65ad5e05856a7d8373
mcvholloway <mcvholloway@gmail.com>
2019-05-24 00:02:30 -0500

    switched to classifier chain

---
+++ category-prediction/classifier_chain.py
@@ -0,0 +1,46 @@
+from scipy import sparse
+import numpy as np
+
+class ClassifierChain:
+    def __init__(self, model):
+        self.model = model
+
+    def fit(self, X, y):
+        counts = y.sum(axis = 0)
+        self.order_ = np.argsort(counts)[::-1]
+        models = [None] * y.shape[1]
+        
+        for i in self.order_:
+            print(i)
+            model = self.model()
+            models[i] = model.fit(X, y[:,i])
+            try: 
+                X = np.hstack([X, model.predict(X).reshape(-1,1)])
+            except:
+                X = sparse.hstack([X, model.predict(X).reshape(-1,1)])
+        
+        self.models_ = models
+        
+    def predict(self, X):
+        predictions = [None]*len(self.order_)
+        for i in self.order_:
+            prediction = (self.models_[i].predict(X)).reshape(-1,1)
+            predictions[i] = prediction
+            try:
+                X = np.hstack([X, prediction])
+            except:
+                X = sparse.hstack([X, prediction])
+            
+        return np.concatenate(predictions, axis = 1)
+
+    def predict_proba(self, X):
+        predictions = [None]*len(self.order_)
+        for i in self.order_:
+            prediction = (self.models_[i].predict_proba(X))[:,1].reshape(-1,1)
+            predictions[i] = prediction
+            try:
+                X = np.hstack([X, prediction])
+            except:
+                X = sparse.hstack([X, prediction])
+            
+        return np.concatenate(predictions, axis = 1)   
--- category-prediction/script.py
+++ category-prediction/script.py
@@ -9,7 +9,7 @@ import pandas as pd
 from scipy.sparse import load_npz
 from functions.helper_functions import make_lookup_dict, ValuePredictor, FindSimilar, FindTags, preprocess_abstract
 from functions.tagger import Tagger
-from functions.ft_predictor import FTPredictor
+#from functions.ft_predictor import FTPredictor
 from lime.lime_text import LimeTextExplainer
 from sklearn.pipeline import make_pipeline
 
@@ -18,14 +18,18 @@ app = Flask(__name__)
 
 tagger = Tagger()
 
-loaded_model = ft.load_model('model_abstract.bin')
+#loaded_model = ft.load_model('model_abstract.bin')
+#loaded_model = ft.load_model('../models/ft_model.bin')
 
-ft_predictor = FTPredictor(loaded_model)
+#ft_predictor = FTPredictor(loaded_model)
 
-with open('models/model_pipeline.pkl', 'rb') as file:
+#with open('../models/model_pipeline.pkl', 'rb') as file:
+#    pipeline = pickle.load(file)
+
+with open('../models/cc_model_pipeline.pkl', 'rb') as file:
     pipeline = pickle.load(file)
 
-with open('models/mlb.pkl', 'rb') as file:
+with open('../models/mlb.pkl', 'rb') as file:
     mlb = pickle.load(file)
 
 with open('../trained_models/tf_vect.pickle', 'rb') as pickle_file:
@@ -56,20 +60,22 @@ def result():
         to_predict_list = list(to_predict_list.values())
         #to_predict_list = list(map(int, to_predict_list))
 
-        #preprocessed = preprocess_abstract(to_predict_list[0] + '. ' + to_predict_list[1])
-        preprocessed = (to_predict_list[0] + '. ' + to_predict_list[1]).replace('\n', ' ')
+        preprocessed = preprocess_abstract(to_predict_list[0] + '. ' + to_predict_list[1])
+        #preprocessed = (to_predict_list[0] + '. ' + to_predict_list[1]).replace('\n', ' ')
 
-        #prediction = ValuePredictor(preprocessed, pipeline, mlb, loaded_model)
+        prediction_pre = ValuePredictor(preprocessed, pipeline, mlb)
 
 
-        prediction_pre = ValuePredictor(preprocessed, ft_predictor, mlb)
-        related_docs_indices, related_docs_similarities = FindSimilar(to_predict_list, tf_vect, tfidf)
+        #prediction_pre = ValuePredictor(preprocessed, ft_predictor, mlb)
+        #related_docs_indices, related_docs_similarities = FindSimilar(to_predict_list, tf_vect, tfidf)
+        related_docs_indices, related_docs_similarities = FindSimilar(preprocessed, tf_vect, tfidf)
+        
 
         lookup_dict = make_lookup_dict()
         prediction = [lookup_dict[x] for x in prediction_pre]
         #prediction = ft_predictor.predict(to_predict_list[0] + '. ' + to_predict_list[1])
         #prediction = [x for x in mlb.inverse_transform(prediction)[0]]
-        tags = tagger.generate_tags(to_predict_list[0] + ' ' + to_predict_list[1])
+        tags = tagger.generate_tags(to_predict_list[0] + '. ' + to_predict_list[1])
 
         #global prediction = prediction
         #global to_predict_list = to_predict_list
@@ -88,196 +94,8 @@ def tags():
         #return render_template("index.html")
         return render_template("result.html",prediction=prediction, related_docs_indices = related_tags, df = df, tags = tags, to_predict_list = to_predict_list, related_docs_similarities = related_tags_similarities, num = list(range(len(related_tags))), lookup_dict = lookup_dict, prediction_pre = prediction_pre)
 
-@app.route('/AG')
-def AG(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('AG'),))
+@app.route('/<cat>')
+def cat_view(cat): 
+    exp = explainer.explain_instance(preprocessed, pipeline.predict_proba, num_features=6, labels = (list(mlb.classes_).index(cat),))
     html = exp.as_html()
     return render_template("AG.html", html = Markup(html))
-
-@app.route('/AT', methods = ['POST'])
-def AT():
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('AT'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = html)
-
-@app.route('/AP')
-def AP(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('AP'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/CT')
-def CT(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('CT'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/CA')
-def CA(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('CA'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/CO')
-def CO(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('CO'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/AC')
-def AC(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('AC'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/CV')
-def CV(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('CV'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/DG')
-def DG(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('DG'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/DS')
-def DS(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('DS'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/FA')
-def FA(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('FA'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/GM')
-def GM(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('GM'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/GN')
-def GN(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('GN'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/GT')
-def GT(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('GT'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/GR')
-def GR(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('AG'),))
-    html = exp.as_html()
-    return render_template("GR.html", html = Markup(html))
-
-@app.route('/HO')
-def HO(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('HO'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/IT')
-def IT(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('IT'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/KT')
-def KT(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('KT'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/LO')
-def LO(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('LO'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/MP')
-def MP(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('MP'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/MG')
-def MG(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('MG'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/NT')
-def NT(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('NT'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/NA')
-def NA(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('NA'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/OA')
-def OA(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('OA'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/OC')
-def OC(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('OC'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/PR')
-def PR(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('PR'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/QA')
-def QA(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('QA'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/RT')
-def RT(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('RT'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/RA')
-def RA(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('RT'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/SP')
-def SP(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('SP'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/ST')
-def ST(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('ST'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-@app.route('/SG')
-def SG(): 
-    exp = explainer.explain_instance(preprocessed, ft_predictor.predict_proba, num_features=6, labels = (list(mlb.classes_).index('SG'),))
-    html = exp.as_html()
-    return render_template("AG.html", html = Markup(html))
-
-
