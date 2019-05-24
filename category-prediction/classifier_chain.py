ab7200e628a4eba500d992529179ef760a80872a
mcvholloway <mcvholloway@gmail.com>
2019-05-24 00:15:09 -0500

    fixed prediction method

--- category-prediction/classifier_chain.py
+++ category-prediction/classifier_chain.py
@@ -21,17 +21,22 @@ class ClassifierChain:
         
         self.models_ = models
         
+#    def predict(self, X):
+#        predictions = [None]*len(self.order_)
+#        for i in self.order_:
+#            prediction = (self.models_[i].predict(X)).reshape(-1,1)
+#            predictions[i] = prediction
+#            try:
+#                X = np.hstack([X, prediction])
+#            except:
+#                X = sparse.hstack([X, prediction])
+#            
+#        return np.concatenate(predictions, axis = 1)
+
     def predict(self, X):
-        predictions = [None]*len(self.order_)
-        for i in self.order_:
-            prediction = (self.models_[i].predict(X)).reshape(-1,1)
-            predictions[i] = prediction
-            try:
-                X = np.hstack([X, prediction])
-            except:
-                X = sparse.hstack([X, prediction])
-            
-        return np.concatenate(predictions, axis = 1)
+        probs = self.predict_proba(X)
+        predictions = (probs > 0.5).astype(int)
+        return predictions
 
     def predict_proba(self, X):
         predictions = [None]*len(self.order_)
